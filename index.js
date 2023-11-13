896d380762864d0697195062d7b35c7b38b45b5c
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-13 13:57:55 -0500

    labwork

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code heregit@github.com:stevenfmentzer/phase-1-arithmetic-lab.git
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 10;
+let num4 = 6;
+
+let mod = num3 % num4; 
+
+let max = Math.max(1,2,3,20);
